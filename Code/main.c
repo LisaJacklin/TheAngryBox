b8293fadc884ff53b8a4ec2677682b35887a37cc
Lisa Jacklin <lmjacklin@alaska.edu>
2023-03-30 12:27:47 -0800

    LCd_testing
    has not been debugged or tested. simply getting functions out to mess with

--- Code/main.c
+++ Code/main.c
@@ -1,7 +1,36 @@
-#include <__cross_studio_io.h>
-void
-main(void)
+/*
+EE 444 Embedded Systems 
+Group 4 Project: Reverse Bop-it
+
+LCD_testing.c
+Coded by: Lisa Jacklin
+
+*/
+
+#include <msp430.h>
+#include <hal_lcd.h> //LCD library header file
+
+void main(void)
 {
-  debug_printf("hello world\n");
-  debug_exit(0);
+  //first, you must initialize the LCD screen
+  halLcdInit();
+  //then you must initialize the backlight
+  halLcdBackLightInit();
+
+  //now to set backlight level and contast level
+  halLcdSetBackLight();// this takes a unsigned char
+  halLcdSetContrast(); //also takes an unsigned char
+
+  /*
+  //Since we are trying to save power, there are several function prototypes to attempt and use
+  halLcdShutDown();
+  halLcdShutDownBackLight();
+  halLcdStandby();
+  halLcdActive();
+
+  //items that could help to clear anything to send another message (or like up for 5, then down till prompted)
+  halLcdClearScreen();
+
+  */
+
 }
