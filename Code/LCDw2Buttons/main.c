c8a981fcac3f098b0961e764a65a429ea0200282
Lisa Jacklin <lmjacklin@alaska.edu>
2023-04-06 16:43:40 -0800

    LCD working and tested

--- Code/LCDw2Buttons/main.c
+++ Code/LCDw2Buttons/main.c
@@ -3,26 +3,21 @@ EE 444 Project Team 4
 Reverse Bop-it
 4/6/2023
 
-LCDwISR.c
+LCDw2Buttons.c
 Coded by: Lisa Jacklin
-Objective: learn to use the LCD screen on the MSP430 with a simple button interrupt to display a message
+Objective: setting up interrupts to display different messages based on two different buttons that are being pressed
 
 Operations:
 - button press working correctly to light up an LED and LCD screen is initialized.
+-
 
 */
 
 #include <msp430.h>
 #include <hal_lcd.h>
 
-//for testing purposes, incrementing the Vcore could be performed....
-
-int time = 0;
-
 void main(void) {
 
-
-//basics for the LCD screen
 ////////////////////LCD SCREEN SETUP//////////////////////////////
 
 halLcdInit();
@@ -30,7 +25,6 @@ halLcdBackLightInit();
 
 //we had previously discussed adjusting this to work 
 halLcdSetBackLight(10);
-//halLcdSetContrast(45);
 
 //now to setup the lcd to be in standby so it can be used later in the ISR
 halLcdStandby();
@@ -38,10 +32,10 @@ halLcdStandby();
 ////////////////////BUTTON SETUP//////////////////////////////
 
  //BUTTON SETUP
-    P2DIR &= ~BIT7;                           //sets to input direction
-    P2IE |= BIT7;                             // interrupts are allowed for this button                  
-    P2REN = BIT7;                             //input with pull up resistor 
-    P2OUT = BIT7;                             //also required to enable pull up resistor.
+    P2DIR &= ~BIT7 + BIT6;                    //sets to input direction
+    P2IE |= BIT7 + BIT6;                      // interrupts are allowed for this button                  
+    P2REN = BIT7 + BIT6;                      //input with pull up resistor 
+    P2OUT = BIT7 + BIT6;                      //also required to enable pull up resistor.
 
  //FLASHING LED TO TEST THE ISR BEFORE/DURING LCD SCREEN WORK
   P1DIR |= BIT0;                             //pin set to output 
@@ -55,8 +49,9 @@ halLcdStandby();
 }
 // BUTTON INTERRUPT TO TEST THE LCD SCREEN WITH
 void Interrupt_Button(void) __interrupt [PORT2_VECTOR] {
+  //button 2.7
 
-    if (P2IV == P2IV_P2IFG7){
+    if (P2IV == P2IV_P2IFG7){ 
       //toggling LED to show the LCD is now active
        P1OUT ^= BIT0;    
          halLcdActive();
@@ -71,5 +66,30 @@ void Interrupt_Button(void) __interrupt [PORT2_VECTOR] {
              halLcdClearScreen();
        P1OUT &=~ BIT0;  
         // halLcdStandby();
-    }   
+      P2IFG &= ~BIT7; // Clear interrupt flag for P2.7
+    }  
+
+  /*
+      //button 2.6
+    if (P2IV == P2IV_P2IFG6) {
+     //toggling LED to show the LCD is now active
+       P1OUT ^= BIT0;    
+         halLcdActive();
+       P1OUT &=~ BIT0;                         
+
+       halLcdPrint("LEAVE ME BE ", 'B'); //won't lie, no idea why the b but a style text char is needed so B it is
+
+      __delay_cycles(600000);
+
+       //and toggling LED to display the cleared screen 
+       P1OUT ^= BIT0;    
+             halLcdClearScreen();
+       P1OUT &=~ BIT0;  
+        // halLcdStandby();
+        P2IFG &= ~BIT6; // Clear interrupt flag for P2.6
+    }
+
+    */
+
+     
 }
