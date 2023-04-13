0c2bc460289e750d6a7971fbfae4055aa65c066e
Lisa Jacklin <lmjacklin@alaska.edu>
2023-04-13 15:01:36 -0800

    accelerator trying

---
+++ Code/AccelSetup/main.c
@@ -0,0 +1,15 @@
+/* EE 444 Embedded Systems
+Project: Reverse Bop-it
+AccelBegin.c
+
+Objective: get the accelerometer to operate correctly and as expected for the 
+
+*/
+
+#include <msp430.h>
+
+void main(void)
+{
+
+
+}
