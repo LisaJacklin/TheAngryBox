d92b26c5727dc7eacc3a470dea11790677ef3c08
Lisa Jacklin <lmjacklin@alaska.edu>
2023-04-20 15:21:30 -0800

    updates

--- Code/AccelSetup/accelerometerproject.c
+++ Code/AccelSetup/accelerometerproject.c
@@ -15,13 +15,22 @@ UCSCTL2 |= 61;                     // 2 MHz sorta
 UCSCTL3 |= SELREF__XT1CLK;     //  
 UCSCTL4 |= SELM__DCOCLK + SELS__DCOCLKDIV;    //SMCLK SOURCED FROM XT1CLK
 
+//FREQUENCY CHECKING
+   P11DIR |= BIT1;                                 //SET TO BE ABLE TO CHECK THE FREQUENCY
+   P11SEL |= BIT1;    
+
 //current: depends on ODR (50 Hz) --> 24 uA = Idd
 
 //output data rates (ODR): register 0x2A where DR[0:2]=100 (50 Hz or 20 ms) or bits 3 through 5
 
 //select SCL and SDA bits
 
-P3SEL |= BIT1 + BIT2;     //select SDA and SCL (both either used as inputs or outputs) set up as input
+//new forma for SDA AND SCL bits
+P3SEL = (BIT1 + BIT2);
+P3DIR |=BIT1; //TRANSMIT OR OUTPUT SDA PIN
+P3DIR &=~ BIT2; //RECIEVE OR INPUT SCL PIN
+
+//P3SEL |= BIT1 + BIT2;     //select SDA and SCL (both either used as inputs or outputs) set up as input
 //P3REN |= BIT1 + BIT2;     //set up pullup resistors for connecting to power supply (positive)
 
 P11SEL |= BIT2 + BIT1;
@@ -33,10 +42,10 @@ UCB0CTL1 |= UCSWRST;    //ENABLE RESET
 
 UCB0CTL0 |= UCMST + UCMODE_3 + UCSYNC;//set to master in I2C mode and synchronous, both adresses are 7 bits
 
+UCB0CTL1 |= UCSSEL_3;  //smclk, reciever, acknowledge normally
+
 UCB0BR0 |= 40; //baud rate = 50 kHz from 2 MHz
 UCB0BR1 |= 0;
-
-UCB0CTL1 |= UCSSEL_3;  //smclk, reciever, acknowledge normally
 
 UCB0CTL1 &= ~UCSWRST;    // disable reset
 
@@ -50,33 +59,27 @@ UCB0CTL1 &= ~UCSWRST;    // disable reset
 
 //transmit adresses//
 
-//SINCE SA0 ON THE BOARD IS SET TO 1, THE ADDRESS IS 3B FOR THE SLAVE
-UCB0I2CSA = 0x3B;      // write and SA0=1
+  //SINCE SA0 ON THE BOARD IS SET TO 1, THE ADDRESS IS 3B FOR THE SLAVE
+  UCB0I2CSA = 0x1D;      // write and SA0=1
+  UCB0CTL1 |= UCTXSTT + UCTR; //start condition generated and I2C transmit 
+  UCB0TXBUF = 0x06; //address of the Z_LSB Register
 
-UCB0CTL1 |= UCTXSTT + UCTR; //start condition generated and I2C transmit 
+  while(!(UCB0IFG & UCTXIFG));  //wait for register address to be sent
+  while(UCB0CTL1 & UCTXSTT);  //wait for start bit cleared
 
-UCB0TXBUF = 0x06; //address of the Z_LSB Register
+  //Recieve z-axis force//
+  UCB0CTL1 |= UCTXSTT;  //generate start again
+  UCB0CTL1 &= ~UCTR;  //recieve mode
+  UCB0I2CSA = 0x1D;      // write and SA0=1
+  while(UCB0CTL1 & UCTXSTT);  //wait for start bit cleared
 
-while(!(UCB0IFG & UCTXIFG));  //wait for register address to be sent
-while(UCB0CTL1 & UCTXSTT);  //wait for start bit cleared
+  z = UCB0RXBUF;  //read z-axis byte
 
-//Recieve z-axis force//
+  while(!(UCB0IFG & UCRXIFG));  //wait to recieve value
 
-UCB0CTL1 |= UCTXSTT;  //generate start again
+  UCB0CTL1 |= UCTXSTP;  //generate stop
 
-UCB0CTL1 &= ~UCTR;  //recieve mode
-
-while(UCB0CTL1 & UCTXSTT);  //wait for start bit cleared
-
-z = UCB0RXBUF;  //read z-axis byte
-
-while(!(UCB0IFG & UCRXIFG));  //wait to recieve value
-
-UCB0CTL1 |= UCTXSTP;  //generate stop
-
-while(UCB0CTL1 & UCTXSTT);  //wait for stop to be sent
-
-
+  while(UCB0CTL1 & UCTXSTT);  //wait for stop to be sent
 
 }
 
--- Code/Reverse-Bopit/main.c
+++ Code/Reverse-Bopit/main.c
@@ -1,7 +1,71 @@
-#include <__cross_studio_io.h>
-void
-main(void)
-{
-  debug_printf("hello world\n");
-  debug_exit(0);
+/*
+EE 444 Embedded Systems
+Reverse Bop-it
+
+Compiled full program
+Team Members: 
+  Audrey Eikenberry
+  Jessica Pennock
+  Lisa Jacklin
+*/
+
+#include <msp430.h>
+#include <hal_lcd.h> //LCD library header file
+
+void main(void) {
+////////////////////LCD SCREEN SETUP//////////////////////////////
+
+halLcdInit();
+halLcdBackLightInit();
+
+//we had previously discussed adjusting this to work which can still be done, 
+//but power should be considered when doing this stuff.
+halLcdSetBackLight(10);
+
+//now to setup the lcd to be in standby so it can be used later in the ISR
+halLcdStandby();
+
+////////////////////BUTTON SETUP//////////////////////////////
+
+ //BUTTON SETUP
+    P2DIR &= ~BIT7 + BIT6;                    //sets to input direction
+    P2IE |= BIT7 + BIT6;                      // interrupts are allowed for this button                  
+    P2REN = BIT7 + BIT6;                      //input with pull up resistor 
+    P2OUT = BIT7 + BIT6;                      //also required to enable pull up resistor.
+
+ //FLASHING LED TO TEST THE ISR BEFORE/DURING LCD SCREEN WORK
+  P1DIR |= BIT0;                             //pin set to output 
+  P1SEL &= ~BIT0;                            //set pin to I/O mode
+
+
+///////////////INITIATING THE CLOCK /////////////////////
+
+/////////////SETTING UP THE SLAVE/MASTER //////////////////
+
+////////////INTERRUPTS AND POWER MODE ENABLES/////////////
+ _EINT();
+ LPM0;
 }
+
+// BUTTON INTERRUPT TO TEST THE LCD SCREEN WITH
+void Interrupt_Button(void) __interrupt [PORT2_VECTOR] {
+  //button 2.7
+
+    if (P2IV == P2IV_P2IFG7){ 
+      //toggling LED to show the LCD is now active
+       P1OUT ^= BIT0;    
+         halLcdActive();
+       P1OUT &=~ BIT0;                         
+
+       halLcdPrint("STOP POKING ME ", 'B'); //won't lie, no idea why the b but a style text char is needed so B it is
+
+      __delay_cycles(600000);
+
+       //and toggling LED to display the cleared screen 
+       P1OUT ^= BIT0;    
+             halLcdClearScreen();
+       P1OUT &=~ BIT0;  
+        // halLcdStandby();
+      P2IFG &= ~BIT7; // Clear interrupt flag for P2.7
+    }  
+
